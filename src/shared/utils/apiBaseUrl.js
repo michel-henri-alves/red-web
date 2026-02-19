97df9aa5f879e4c783cb14ba16518aad05e2f6a3
Michel Henrique Alves <michel.alves@sensedia.com>
2026-02-18 21:00:49 -0300

    [chore/#2] adicionado baseUrl para local e cloud

--- src/shared/utils/apiBaseUrl.js
+++ src/shared/utils/apiBaseUrl.js
@@ -6,9 +6,9 @@ export default function axiosClient(domain) {
     // let port = import.meta.env.VITE_BACKEND_PORT
 
     return axios.create({
-        baseURL: import.meta.env.VITE_API_BASE_URL + '/' + domain,
-        // baseURL: `${url}:${port}/${domain}`, 
-        // baseURL: 'http://192.168.1.167:3001/' + domain,
-        // baseURL: `${import.meta.env.VITE_API_BASE_URL}/${domain}`,
+       //cloud
+        // baseURL: import.meta.env.VITE_API_BASE_URL + '/' + domain,
+        //local
+        baseURL: 'http://192.168.1.167:3001/' + domain,
     })
 }
